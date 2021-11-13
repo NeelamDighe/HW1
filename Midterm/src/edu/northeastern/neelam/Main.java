24b9c25a1af34a1ba4d36ea13254020fa8818c2e
NeelamDighe <digheneelam@gmail.com>
2021-11-12 23:25:44 -0800

    assignment 4 - Q1

---
+++ Midterm/src/edu/northeastern/neelam/Main.java
@@ -0,0 +1,8 @@
+package edu.northeastern.neelam;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ mood/src/edu/northeastern/neelam/Happyobject.java
@@ -0,0 +1,19 @@
+package edu.northeastern.neelam;
+
+
+    public class Happyobject extends Moodyobject{
+        public  String getMood() {
+
+            return "Happy";
+        }
+
+        public void expressFeelings() {
+            System.out.println("heeehee….hahahah…HAHAHA!!");
+
+        }
+        public String toString(){
+            return "Subject laughs a lot";
+        }
+
+
+    }
